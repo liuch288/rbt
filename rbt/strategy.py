b1d18b576128eab1f8449923b4959892eefd1de9
ranglab <lc_zd@163.com>
2024-10-31 20:39:53 +0800

    change name

--- rbt/strategy.py
+++ rbt/strategy.py
@@ -1,3 +1,5 @@
+from progressbar import ProgressBar
+
 from .md_engine import MdEngine
 from .dmu import DecisionMakingUnit
 from .peu import PnlEstimateUnit
@@ -18,9 +20,13 @@ class Strategy(object):
     def register_md_engine(self, md_engine: MdEngine):
         self.md_engine = md_engine
 
-    def run(self):
+    def run(self, show_progress:bool=False):
         self.dmu_results = {}
         self.pnl_estimations = {}
+        if show_progress:
+            bar = ProgressBar(maxval=len(self.md_engine.raw_md))
+            bar.start()
+            step_count = 0
         while True:
             new_md = self.md_engine.get_current_md()
             if new_md is None:
@@ -29,7 +35,7 @@ class Strategy(object):
 
             cur_dmu_results = {}
             for dmu in self.dmus:
-                dmu_name = dmu.__class__.__name__
+                dmu_name = dmu.name
                 result = dmu.on_market_data(new_md)
                 for key in result.keys():
                     cur_dmu_results[f"{dmu_name}_{key}"] = result[key]
@@ -37,7 +43,7 @@ class Strategy(object):
 
             cur_peu_results = {}
             for peu in self.peus:
-                peu_name = peu.__class__.__name__
+                peu_name = peu.name
                 future_md = self.md_engine.get_future_md(
                     peu.watching_time, peu.watching_mds
                 )
@@ -47,3 +53,9 @@ class Strategy(object):
             self.pnl_estimations[cur_time] = cur_peu_results
             if not self.md_engine.finish_current_md():
                 break
+            
+            if show_progress:
+                step_count += 1
+                bar.update(step_count)
+        if show_progress:
+            bar.finish()
