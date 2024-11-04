90f5daa4318c082cd16d73cd23d3b2502795e830
ranglab <lc_zd@163.com>
2024-11-05 01:39:12 +0800

    update

--- rbt/strategy.py
+++ rbt/strategy.py
@@ -21,8 +21,7 @@ class Strategy(object):
         self.md_engine = md_engine
 
     def run(self, show_progress:bool=False):
-        self.dmu_results = {}
-        self.pnl_estimations = {}
+        self.unit_results = {}
         if show_progress:
             bar = ProgressBar(maxval=len(self.md_engine.raw_md))
             bar.start()
@@ -33,24 +32,23 @@ class Strategy(object):
                 break
             cur_time = new_md.name
 
-            cur_dmu_results = {}
+            unit_results = {}
             for dmu in self.dmus:
                 dmu_name = dmu.name
-                result = dmu.on_market_data(new_md, cur_dmu_results)
+                result = dmu.on_market_data(new_md, unit_results)
                 for key in result.keys():
-                    cur_dmu_results[f"{dmu_name}_{key}"] = result[key]
-            self.dmu_results[cur_time] = cur_dmu_results
+                    unit_results[f"{dmu_name}_{key}"] = result[key]
 
-            cur_peu_results = {}
             for peu in self.peus:
                 peu_name = peu.name
                 future_md = self.md_engine.get_future_md(
                     peu.watching_time, peu.watching_mds
                 )
-                result = peu.estimate(future_md)
+                result = peu.estimate(future_md, unit_results)
                 for key in result.keys():
-                    cur_peu_results[f"{peu_name}_{key}"] = result[key]
-            self.pnl_estimations[cur_time] = cur_peu_results
+                    unit_results[f"{peu_name}_{key}"] = result[key]
+            self.unit_results[cur_time] = unit_results
+            
             if not self.md_engine.finish_current_md():
                 break
             
