bb53ba6ce0fc6e61ab4f049d41b013b1eef8d82b
ranglab <lc_zd@163.com>
2024-11-13 07:21:48 +0000

    update rbt/peu/biside_quote_mos_peu.py.

--- rbt/peu/biside_quote_mos_peu.py
+++ rbt/peu/biside_quote_mos_peu.py
@@ -3,6 +3,46 @@ import pandas as pd
 from .pnl_estimate_unit import PnlEstimateUnit
 
 
+class Order(object):
+    """
+    direction: [1] buy; [-1] sell
+    """
+
+    def __init__(
+        self, price: float, volume: int, direction: int, volume_before_this_order: int
+    ):
+        self.price = price
+        self.volume = volume
+        self.direction = direction
+        self.volume_before_this_order = volume_before_this_order
+
+    def check_execution(self, market_order: dict) -> dict:
+        """
+        market_order: {'side': 'sell', 'price': 102.09, 'volume': 14}
+        return: {"volume": int, "cash_flow": float} cash_flow还需要乘以hands才能得到实际notional
+        """
+        # 如果当前订单已经完成，则直接返回0现金流
+        if self.volume <= 0:
+            return {"volume": 0, "cash_flow": 0}
+        # 如果是同向订单，则不会发生成交
+        mo_direction = 1 if market_order["side"] == "buy" else -1
+        if mo_direction * self.direction == 1:
+            return {"volume": 0, "cash_flow": 0}
+        # 只有价格匹配，才会成交
+        if self.direction * (self.price - market_order["price"]) >= 0:
+            mo_vol = market_order["volume"]
+            if mo_vol <= self.volume_before_this_order:
+                self.volume_before_this_order -= mo_vol
+            else:
+                mo_vol -= self.volume_before_this_order
+                self.volume_before_this_order = 0
+                cur_exec = min(self.volume, mo_vol)
+                self.volume -= cur_exec
+                cash_flow = -1 * self.direction * self.price * cur_exec
+                return {"volume": cur_exec, "cash_flow": cash_flow}
+        return {"volume": 0, "cash_flow": 0}
+
+
 class BisideQuoteMosPEU(PnlEstimateUnit):
     def __init__(
         self,
@@ -20,42 +60,71 @@ class BisideQuoteMosPEU(PnlEstimateUnit):
         self.digits = len(str(tick_size).split(".")[1])
 
     def estimate(self, future_data, *args, **kwargs) -> dict:
+        # 确定有多少订单排在前面
         init_md = future_data.iloc[0]
+        # bid
         buy_order_price = round(
             init_md["bid_px1"] - (self.lb - 1) * self.tick_size, self.digits
         )
+        bid_vol_at_same_level = 0
+        for i in range(1, 6):
+            cur_level_price = init_md[f"bid_px{i}"]
+            cur_level_vol = init_md[f"bid_sz{i}"]
+            if cur_level_price == buy_order_price:
+                bid_vol_at_same_level = cur_level_vol
+            elif cur_level_price < buy_order_price:
+                break
+        buy_order = Order(buy_order_price, 1, 1, bid_vol_at_same_level)
+        # ask
         sell_order_price = round(
             init_md["ask_px1"] + (self.la - 1) * self.tick_size, self.digits
         )
+        ask_vol_at_same_level = 0
+        for i in range(1, 6):
+            cur_level_price = init_md[f"bid_px{i}"]
+            cur_level_vol = init_md[f"bid_sz{i}"]
+            if cur_level_price == sell_order_price:
+                ask_vol_at_same_level = cur_level_vol
+            elif cur_level_price > sell_order_price:
+                break
+        sell_order = Order(sell_order_price, 1, -1, ask_vol_at_same_level)
 
-        # -------------------------------------------
-        df = future_data["exec_after"].reset_index()
-        df_expanded = df.explode("exec_after").dropna()
-
-        # 将exec_after中的字典元素展开为列
-        df_expanded = pd.concat(
-            [
-                df_expanded.drop(["exec_after"], axis=1),
-                df_expanded["exec_after"].apply(pd.Series),
-            ],
-            axis=1,
-        )
-        df_expanded
-        # -------------------------------------------
-
-        buy_order_hits = future_data[future_data["ask_px1"] <= buy_order_price]
-        buy_order_executed = len(buy_order_hits) > 0
-
-        sell_order_hits = future_data[future_data["bid_px1"] >= sell_order_price]
-        sell_order_executed = len(sell_order_hits) > 0
-
+        # 逐行核对是否成交
+        inventory = 0
         pnl = 0.0
-        if (buy_order_executed == True) & (sell_order_executed == False):
-            pnl = future_data.iloc[-1]["bid_px1"] - buy_order_price
-        if (buy_order_executed == False) & (sell_order_executed == True):
-            pnl = sell_order_price - future_data.iloc[-1]["ask_px1"]
-        if (buy_order_executed == True) & (sell_order_executed == True):
-            pnl = sell_order_price - buy_order_price
+        future_data_len = len(future_data)
+        # 此处-3是因为最后3行要用于判定平仓价格
+        buy_order_executed = False
+        sell_order_executed = False
+        for i in range(future_data_len - 3):
+            exec_after = future_data.iloc[i]["exec_after"]
+            for exec in exec_after:
+                if not buy_order_executed:
+                    res = buy_order.check_execution(exec)
+                    if res["volume"] > 0:
+                        inventory += res["volume"]
+                        buy_order_executed = True
+                        pnl -= res["cash_flow"]
+                if not sell_order_executed:
+                    res = sell_order.check_execution(exec)
+                    if res["volume"] > 0:
+                        inventory -= res["volume"]
+                        sell_order_executed = True
+                        pnl += res["cash_flow"]
+            if buy_order_executed and sell_order_executed:
+                break
+        # 平仓
+        if buy_order_executed ^ sell_order_executed:
+            for i in range(future_data_len - 3, future_data_len):
+                cur_md = future_data.iloc[i]
+                cur_spread = round(
+                    (cur_md["ask_px1"] - cur_md["bid_px1"]) / self.tick_size
+                )
+                if (cur_spread < 2) or (i == future_data_len - 1):
+                    if inventory > 0:
+                        pnl += cur_md["bid_px1"] * inventory
+                    else:
+                        pnl -= cur_md["ask_px1"] * inventory
 
         cur_result = {
             "pnl": pnl,
