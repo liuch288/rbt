4729b27e41e2499cecb7b9bdf517bb9810dbe1af
ranglab <lc_zd@163.com>
2024-11-14 07:07:03 +0000

    Revert "update rbt/ic/recover_mos.py."
    This reverts commit e4cce475abbdc8b11d25a19d0c670888048f6e32.

--- rbt/ic/recover_mos.py
+++ rbt/ic/recover_mos.py
@@ -1,154 +1,172 @@
-import pandas as pd
+import cvxpy as cp
+import numpy as np
 
-from .pnl_estimate_unit import PnlEstimateUnit
+from .index_calculator import IndexCalculator
+from ..util import get_instrument_info
 
 
-class Order(object):
-    """
-    direction: [1] buy; [-1] sell
-    """
+def recover_mo_core(
+    last_lob, cur_lob, tick_size: float, hands: int, split_unknown: bool
+) -> list:
+    trade_notional = cur_lob["trade_notional"]
+    trade_size = cur_lob["trade_sz"]
+    if trade_size == 0:
+        return []
+    avg_price = trade_notional / trade_size / hands
+    tick_notional = round(tick_size * hands)
+    last_mid_px = (last_lob["bid_px1"] + last_lob["ask_px1"]) / 2
+    if avg_price < last_mid_px:
+        avg_price_boundary = (int(avg_price / tick_size) - 1) * tick_size
+    else:
+        avg_price_boundary = (int(avg_price / tick_size) + 2) * tick_size
 
-    def __init__(
-        self, price: float, volume: int, direction: int, volume_before_this_order: int
-    ):
-        self.price = price
-        self.volume = volume
-        self.direction = direction
-        self.volume_before_this_order = volume_before_this_order
+    # price vector
+    min_price = min(last_lob["bid_px2"], avg_price_boundary, cur_lob["bid_px2"])
+    min_price_notional = round(min_price * hands)
+    max_price = max(last_lob["ask_px2"], avg_price_boundary, cur_lob["ask_px2"])
+    max_price_notional = round(max_price * hands)
+    bids = list(
+        range(min_price_notional, round(last_lob["bid_px1"] * hands) + 1, tick_notional)
+    )
+    asks = list(
+        range(round(last_lob["ask_px1"] * hands), max_price_notional + 1, tick_notional)
+    )
+    between_ask_bid = list(range(bids[-1] + tick_notional, asks[0], tick_notional))
+    prices_list = bids + between_ask_bid + asks
+    price_levels = len(prices_list)
 
-    def check_execution(self, market_order: dict) -> dict:
-        """
-        market_order: {'side': 'sell', 'price': 102.09, 'volume': 14}
-        return: {"volume": int, "cash_flow": float} cash_flow还需要乘以hands才能得到实际notional
-        """
-        # 如果当前订单已经完成，则直接返回0现金流
-        if self.volume <= 0:
-            return {"volume": 0, "cash_flow": 0}
-        # 如果是同向订单，则不会发生成交
-        mo_direction = 1 if market_order["side"] == "buy" else -1
-        if mo_direction * self.direction == 1:
-            return {"volume": 0, "cash_flow": 0}
-        # 只有价格匹配，才会成交
-        if self.direction * (self.price - market_order["price"]) >= 0:
-            mo_vol = market_order["volume"]
-            if mo_vol <= self.volume_before_this_order:
-                self.volume_before_this_order -= mo_vol
+    # volume vector
+    last_volume = {}
+    last_volume[round(last_lob[f"bid_px1"] * hands)] = last_lob[f"bid_sz1"] + 10
+    last_volume[round(last_lob[f"ask_px1"] * hands)] = last_lob[f"ask_sz1"] + 10
+    for i in range(2, 6):
+        last_volume[round(last_lob[f"bid_px{i}"] * hands)] = last_lob[f"bid_sz{i}"]
+        last_volume[round(last_lob[f"ask_px{i}"] * hands)] = last_lob[f"ask_sz{i}"]
+    prices_volume = []
+    for px in prices_list:
+        if px in last_volume.keys():
+            prices_volume.append(last_volume[px])
+        else:
+            prices_volume.append(10000)
+
+    # weights
+    weights = np.zeros(price_levels)
+    mid = price_levels / 2 - 0.5
+    if avg_price > last_lob["ask_px1"]:
+        mid += 0.5
+    elif avg_price < last_lob["bid_px1"]:
+        mid -= 0.5
+    for i in range(price_levels):
+        weights[i] = (i - mid) ** 2
+
+    # optimize
+    x = cp.Variable(price_levels, integer=True)
+    a = np.array([prices_list, [1] * price_levels])
+    b = np.array([trade_notional, trade_size])
+    objective = cp.Minimize(
+        cp.sum(cp.pos(x - prices_volume)) * 2.2
+        + cp.sum(cp.pos(x))
+        + cp.sum(cp.multiply(weights, x)) * 2
+    )
+    constriants = [0 <= x, a @ x == b]
+    problem = cp.Problem(objective, constriants)
+    problem.solve(solver=cp.CPLEX, verbose=False)
+    if problem.status != "optimal":
+        print("failed")
+        return []
+
+    # process results
+    decomposed_vol = [round(v) for v in x.value]
+    all_orders = []
+    unknown_exec = []
+    for px in bids:
+        cur_vol = decomposed_vol.pop(0)
+        if cur_vol > 0:
+            all_orders.append({"side": "sell", "price": px / hands, "volume": cur_vol})
+    for px in between_ask_bid:
+        cur_vol = decomposed_vol.pop(0)
+        if cur_vol > 0:
+            order_px = px / hands
+            if order_px <= cur_lob["bid_px1"]:
+                direction = "buy"
+            elif order_px >= cur_lob["ask_px1"]:
+                direction = "sell"
             else:
-                mo_vol -= self.volume_before_this_order
-                self.volume_before_this_order = 0
-                cur_exec = min(self.volume, mo_vol)
-                self.volume -= cur_exec
-                cash_flow = -1 * self.direction * self.price * cur_exec
-                return {"volume": cur_exec, "cash_flow": cash_flow}
-        return {"volume": 0, "cash_flow": 0}
+                direction = "unknown"
+            cur_record = {"side": direction, "price": order_px, "volume": cur_vol}
+            if split_unknown and direction == "unknown":
+                unknown_exec.append(cur_record)
+            else:
+                all_orders.append(cur_record)
+    for px in asks:
+        cur_vol = decomposed_vol.pop(0)
+        if cur_vol > 0:
+            all_orders.append({"side": "buy", "price": px / hands, "volume": cur_vol})
 
-
-class BisideQuoteMosPEU(PnlEstimateUnit):
-    def __init__(
-        self,
-        watching_time: float = None,
-        watching_mds: int = None,
-        lb: int = 1,
-        la: int = 1,
-        tick_size: float = 0.005,
-        name: str = None,
-    ):
-        """
-        lb、la是以一档为基准的价格，取1时是指在一档挂单，0时则为比一档更优一个报价单位的价格下单，其他是在一档的基础上加减lx-1个报价单位
-        """
-        super().__init__(watching_time, watching_mds, name)
-        self.lb = lb
-        self.la = la
-        self.tick_size = tick_size
-        self.digits = len(str(tick_size).split(".")[1])
-
-    def estimate(self, future_data, *args, **kwargs) -> dict:
-        # 确定有多少订单排在前面
-        init_md = future_data.iloc[0]
-        # bid
-        buy_order_price = round(
-            init_md["bid_px1"] - (self.lb - 1) * self.tick_size, self.digits
-        )
-        bid_vol_at_same_level = 0
-        for i in range(1, 6):
-            cur_level_price = init_md[f"bid_px{i}"]
-            cur_level_vol = init_md[f"bid_sz{i}"]
-            if cur_level_price == buy_order_price:
-                bid_vol_at_same_level = cur_level_vol
-            elif cur_level_price < buy_order_price:
-                break
-        buy_order = Order(buy_order_price, 1, 1, bid_vol_at_same_level)
-        # ask
-        sell_order_price = round(
-            init_md["ask_px1"] + (self.la - 1) * self.tick_size, self.digits
-        )
-        ask_vol_at_same_level = 0
-        for i in range(1, 6):
-            cur_level_price = init_md[f"bid_px{i}"]
-            cur_level_vol = init_md[f"bid_sz{i}"]
-            if cur_level_price == sell_order_price:
-                ask_vol_at_same_level = cur_level_vol
-            elif cur_level_price > sell_order_price:
-                break
-        sell_order = Order(sell_order_price, 1, -1, ask_vol_at_same_level)
-
-        # 逐行核对是否成交
-        inventory = 0
-        pnl = 0.0
-        future_data_len = len(future_data)
-        # 此处-3是因为最后3行要用于判定平仓价格
-        buy_order_executed = False
-        sell_order_executed = False
-        for i in range(future_data_len - 3):
-            cur_md = future_data.iloc[i]
-            # 首先根据盘口价格判定，看是否能立即成交 (根据盘口价格判定成交时不管量的情况)
-            if not buy_order_executed:
-                cur_ask1 = cur_md["ask_px1"]
-                if cur_ask1 <= buy_order_price:
-                    inventory += buy_order.volume
-                    pnl -= buy_order.volume * buy_order_price
-                    buy_order_executed = True
-            if not sell_order_executed:
-                cur_bid1 = cur_md["bid_px1"]
-                if cur_bid1 >= sell_order_price:
-                    inventory -= sell_order.volume
-                    pnl += sell_order.volume * sell_order_price
-                    sell_order_executed = True
-
-            # 然后根据后续市价单判定
-            exec_after = cur_md["exec_after"]
-            for exec in exec_after:
-                if not buy_order_executed:
-                    res = buy_order.check_execution(exec)
-                    if res["volume"] > 0:
-                        inventory += res["volume"]
-                        buy_order_executed = True
-                        pnl += res["cash_flow"]
-                if not sell_order_executed:
-                    res = sell_order.check_execution(exec)
-                    if res["volume"] > 0:
-                        inventory -= res["volume"]
-                        sell_order_executed = True
-                        pnl += res["cash_flow"]
-            if buy_order_executed and sell_order_executed:
-                break
-        # 平仓
-        if buy_order_executed ^ sell_order_executed:
-            for i in range(future_data_len - 3, future_data_len):
-                cur_md = future_data.iloc[i]
-                cur_spread = round(
-                    (cur_md["ask_px1"] - cur_md["bid_px1"]) / self.tick_size
+    # split unkown orders
+    if split_unknown and unknown_exec:
+        all_buy_volume = 0
+        all_sell_volume = 0
+        for ord in all_orders:
+            if ord["side"] == "buy":
+                all_buy_volume += ord["volume"]
+            elif ord["side"] == "sell":
+                all_sell_volume += ord["volume"]
+        all_classified_volume = all_buy_volume + all_sell_volume
+        all_unknown_volume = 0
+        for ord in unknown_exec:
+            all_unknown_volume += ord["volume"]
+        if all_buy_volume + all_sell_volume > 0:
+            buy_order_ratio = all_buy_volume / all_classified_volume
+        else:
+            buy_order_ratio = 0.5
+        if all_unknown_volume <= all_classified_volume:
+            assigned_buy_volume = round(all_unknown_volume * buy_order_ratio)
+        else:
+            assigned_buy_volume = (
+                all_buy_volume
+                + (all_unknown_volume - all_classified_volume) // 2
+                + round(buy_order_ratio)
+            )
+        for exec in unknown_exec:
+            cur_total = exec["volume"]
+            cur_buy = min(cur_total, assigned_buy_volume)
+            assigned_buy_volume -= cur_buy
+            cur_sell = cur_total - cur_buy
+            if cur_buy > 0:
+                all_orders.append(
+                    {"side": "buy", "price": exec["price"], "volume": cur_buy}
                 )
-                if (cur_spread < 2) or (i == future_data_len - 1):
-                    if inventory > 0:
-                        pnl += cur_md["bid_px1"] * inventory
-                    else:
-                        pnl += cur_md["ask_px1"] * inventory
-                    break
+            if cur_sell > 0:
+                all_orders.append(
+                    {"side": "sell", "price": exec["price"], "volume": cur_sell}
+                )
+        all_orders.sort(key=lambda x: x["price"])
 
-        cur_result = {
-            "pnl": pnl,
-            "buy_order_executed": buy_order_executed,
-            "sell_order_executed": sell_order_executed,
-        }
-        return cur_result
+    return all_orders
+
+
+class RecoverMos(IndexCalculator):
+    def __init__(self, tick_size: float=0.005, hands: int=10000, sym: str = None):
+        super().__init__(1)
+        self.last_lob = None
+        if sym is None:
+            self.tick_size = tick_size
+            self.hands = hands
+        else:
+            info = get_instrument_info(sym)
+            self.tick_size = info["tick_size"]
+            self.hands = info["hands"]
+
+
+    def calculate(self, new_data):
+        if self.last_lob is None:
+            self.result = []
+        else:
+            if self.last_lob.name.hour < 12 and new_data.name.hour > 12:
+                self.result = []
+            else:
+                self.result = recover_mo_core(
+                    self.last_lob, new_data, self.tick_size, self.hands, True
+                )
+        self.last_lob = new_data
