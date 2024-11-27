18cd11280beaeebc3fe93bdefdac86b1f2625717
ranglab <lc_zd@163.com>
2024-11-27 05:39:14 +0000

    update rbt/ic/price_smooth_ic.py.

--- rbt/ic/price_smooth_ic.py
+++ rbt/ic/price_smooth_ic.py
@@ -1,5 +1,6 @@
 from rbt.ic import IndexCalculator
 
+
 class PriceSmoothIC(IndexCalculator):
     """
     恒纪元: 返回恒定价
@@ -8,25 +9,20 @@ class PriceSmoothIC(IndexCalculator):
     乱纪元: P_{t-2} != P_{t-1} != P_{t}
     变为恒纪元: P_{t-1} == P_{t}
     """
-    def __init__(self, threshold):
-        super().__init__(1)
-        self.steady_era = False  # True: 恒纪元, False: 乱纪元
-        self.steady_price = 0.0
-        self.challenging_price = 0.0
-        
+
+    def __init__(self):
+        super().__init__(2)
+        # self.stable_era = False  # True: 恒纪元, False: 乱纪元
 
     def calculate(self, new_data):
-        if self.challenging_price == new_data:
-            self.steady_era = True
-            self.steady_price = new_data
-        elif self.steady_price == new_data:
-            self.steady_era
-        
-            
-        self.result = self.steady_price if self.steady_era else new_data
-
+        """
+        当处于恒纪元时，需要三次价格都不同才进入乱纪元；
+        当处于乱纪元时，只要价格连续两次相同，就进入恒纪元
+        """
+        if self.data[0] == self.data[1]:
+            self.result = self.data[0]
+        else:
+            self.result = new_data
+                
     def reset(self):
         super().reset()
-        self.steady_era = False
-        self.steady_price = 0.0
-        self.challenging_price = 0.0
