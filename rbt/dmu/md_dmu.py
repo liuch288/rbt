c41fec8ddd5bd181ced9f0bbe01757821d90d478
ranglab <lc_zd@163.com>
2024-12-02 02:55:29 +0000

    !12 RBT-21_variance_ic * Merge branch 'master' of https://gitee.com/ranglab/rbt into RBT-21_variance_ic * update rbt/dmu/md_dmu.py. * update rbt/dmu/md_dmu.py. * add rbt/ic/variance_ic.py.

--- rbt/dmu/md_dmu.py
+++ rbt/dmu/md_dmu.py
@@ -1,4 +1,5 @@
 from .decision_making_unit import DecisionMakingUnit
+from ..ic import PriceSmoothIC, VarianceIC, MeanIC
 from ..util import get_instrument_info
 
 
@@ -7,18 +8,32 @@ class MdDMU(DecisionMakingUnit):
         super().__init__(name)
         info = get_instrument_info(sym)
         self.hands = info["hands"]
+        self.bid_filter = PriceSmoothIC()
+        self.ask_filter = PriceSmoothIC()
+        self.variance_ic = VarianceIC(5)
+        self.mean_ic = MeanIC(5)
 
     def make_decision(self, new_data, *args, **kwargs) -> dict:
         bid_px1 = new_data["bid_px1"]
         bid_sz1 = new_data["bid_sz1"]
         ask_px1 = new_data["ask_px1"]
         ask_sz1 = new_data["ask_sz1"]
+        bid_smo = self.bid_filter.update(bid_px1)
+        ask_smo = self.ask_filter.update(ask_px1)
+        mid = (bid_smo + ask_smo) / 2
+        mean_px = round(self.mean_ic.update(mid), 4)
+        std = round(self.variance_ic.update(mid), 9) ** 0.5
+        quantile = (mid - mean_px) / std if std > 1e-8 else 0
         ob_avg = (bid_px1 * ask_sz1 + ask_px1 * bid_sz1) / (bid_sz1 + ask_sz1)
         cum_avg = new_data["tot_notional"] / new_data["tot_sz"] / self.hands
         exec_avg = new_data["trade_notional"] / new_data["trade_sz"] / self.hands
         return {
             "bid": bid_px1,
             "ask": ask_px1,
+            "mid": mid,
+            "mean": mean_px,
+            "std": std,
+            "quantile": quantile,
             "ob_avg": ob_avg,
             "cum_avg": cum_avg,
             "exec_avg": exec_avg,
---
+++ rbt/ic/variance_ic.py
@@ -0,0 +1,16 @@
+from rbt.ic import IndexCalculator, MeanIC
+
+
+class VarianceIC(IndexCalculator):
+    def __init__(self, period):
+        super().__init__(1)
+        self.exp_squared = MeanIC(period)  # 用于计算平方的期望
+        self.exp = MeanIC(period)  # 用于计算期望
+
+    def calculate(self, new_data):
+        """
+        方差 = 平方的期望 - 期望的平方
+        """
+        exp_of_squared_val = self.exp_squared.update(new_data**2)
+        squared_exp_val = self.exp.update(new_data) ** 2
+        self.result = exp_of_squared_val - squared_exp_val
