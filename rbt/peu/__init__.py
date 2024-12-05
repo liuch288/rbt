139595df57471730beb390fb80d97ec5d8a0e192
ranglab <lc_zd@163.com>
2024-12-05 15:46:07 +0000

    !16 RBT-17_biquote_close_stop_peu * Merge branch 'master' of https://gitee.com/ranglab/rbt into RBT-17_biq… * update rbt/peu/__init__.py. * rename rbt/peu/biquote_close_stop_peu.py to rbt/peu/biquote_stop_close_peu.py. * add rbt/peu.

--- rbt/peu/__init__.py
+++ rbt/peu/__init__.py
@@ -1,3 +1,4 @@
 from .biquote_peu import BiquotePEU
 from .biquote_close_peu import BiquoteClosePEU
 from .pnl_estimate_unit import PnlEstimateUnit
+from .biquote_stop_close_peu import BiquoteStopClosePEU
