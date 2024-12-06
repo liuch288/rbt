3eaf1c7a7f87521e2458f833beb440741a1f8fd4
ranglab <lc_zd@163.com>
2024-12-06 02:43:02 +0000

    update rbt/dmu/trend_dmu.py.

--- rbt/dmu/trend_dmu.py
+++ rbt/dmu/trend_dmu.py
@@ -9,16 +9,28 @@ class TrendDMU(DecisionMakingUnit):
         self.ma = MeanIC(period)
         self.smoother = PriceSmoothIC()
         self.last_val = 0.0
+        self.last_direction = 0
 
     def make_decision(self, new_data, prev_result) -> dict:
         mid = (new_data["bid_px1"] + new_data["ask_px1"]) / 2
         mid_smo = self.smoother.update(mid)
         cur_val = self.ma.update(mid_smo)
+        bias = mid_smo - cur_val
         direction = 0
         delta = cur_val - self.last_val
+        #确定当前方向
         if cur_val > self.last_val:
             direction = 1
         elif cur_val < self.last_val:
             direction = -1
         self.last_val = cur_val
-        return {"direction": direction, "delta": delta}
+        # 检测方向变化
+        hits = round((direction - self.last_direction) / 2)
+        self.last_direction = direction
+        return {
+            "hits": hits,
+            "direction": direction,
+            "delta": delta,
+            "ma": cur_val,
+            "bias": bias,
+        }
