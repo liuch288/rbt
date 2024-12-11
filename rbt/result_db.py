3b9d0c01a6939693ebcc1fe0d5b717385185846d
ranglab <lc_zd@163.com>
2024-12-11 22:12:32 +0800

    update

--- rbt/result_db.py
+++ rbt/result_db.py
@@ -1,4 +1,7 @@
+import datetime
 import os
+
+import pandas as pd
 
 
 class ResultDB(object):
@@ -7,11 +10,20 @@ class ResultDB(object):
     ResultDB负责管理已有结果，所有数据以“sym_date.pkl”的形式存放，datetime.da
     """
 
-    def __init__(self, data_path):
-        if os.path.exists(data_path):
-            self.data_path = data_path
+    def __init__(self, db_directory):
+        if os.path.exists(db_directory):
+            self.db_directory = db_directory
         else:
-            raise FileNotFoundError(f"{data_path} not found.")
-    
-    def query_columns():
+            raise FileNotFoundError(f"{db_directory} not found.")
+
+    def query_existed_columns(self, sym: str, date: datetime.date) -> list:
+        path = self._get_path(sym, date)
+        data = pd.read_pickle(path, compression="gzip")
+        return list(data.columns)
+
+    def save_data(self, sym, date, new_data) -> None:
         return None
+
+    def _get_path(self, sym: str, date: datetime.date) -> pd.DataFrame:
+        path = os.path.join(self.db_directory, f"{sym}_{str(date)}.pkl")
+        return path
