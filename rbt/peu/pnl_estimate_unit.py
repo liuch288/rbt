373e0659ec8ae73ae2fe17a7adbe3c647fd6314c
ranglab <lc_zd@163.com>
2024-12-12 02:36:47 +0000

    update rbt/peu/pnl_estimate_unit.py.

--- rbt/peu/pnl_estimate_unit.py
+++ rbt/peu/pnl_estimate_unit.py
@@ -14,6 +14,24 @@ class PnlEstimateUnit(object):
         else:
             self.name = name
 
+    def get_name(self)->str:
+        name = f"{self.__class__.__name__}_{self.__class__.version}"
+        suffix = self.get_param_str()
+        if len(suffix) > 0:
+            name += "_" + suffix
+        return name
+
+    def get_param_str(self) -> str:
+        """生成参数信息，默认为空
+        
+        使用【unit名称-版本号-参数信息】构成PEU唯一标识。
+        对于无参数PEU，子类不需要实现，默认为空。而如果有参数配置，则需要实现参数信息生成函数。
+
+        Returns:
+            str: 载明参数的字符串
+        """
+        return ""
+
     def estimate(self, data, previous_result: dict = {}) -> dict:
         """
         评估交易规则在给定行情数据上的损益
