dcf2eeae7acd8fc820cc72461852444463fd9ad8
ranglab <lc_zd@163.com>
2024-12-13 08:23:37 +0000

    update rbt/peu/pnl_estimate_unit.py.

--- rbt/peu/pnl_estimate_unit.py
+++ rbt/peu/pnl_estimate_unit.py
@@ -1,33 +1,17 @@
-class PnlEstimateUnit:
+from ..unit import Unit
+
+
+class PnlEstimateUnit(Unit):
     version = "v0"
 
     def __init__(self, watching_time: float = None, watching_mds: int = None):
+        super().__init__()
         if watching_time is not None and watching_mds is not None:
             raise ValueError(
                 "Do not input watching_time and watching_mds simultaneously"
             )
         self.watching_time = watching_time
         self.watching_mds = watching_mds
-        self.name = f"{self.__class__.__name__}_{self.__class__.version}"
-        self.name_updated = False
-
-    def update_unit_name(self) -> str:
-        """根据参数更新unit name，由各个PEU主动调用"""
-        suffix = self.get_param_str()
-        if len(suffix) > 0:
-            self.name += "_" + suffix
-        self.name_updated = True
-
-    def get_param_str(self) -> str:
-        """生成参数信息，默认为空
-
-        使用【unit名称-版本号-参数信息】构成PEU唯一标识。
-        对于无参数PEU，子类不需要实现，默认为空。而如果有参数配置，则需要实现参数信息生成函数。
-
-        Returns:
-            str: 载明参数的字符串
-        """
-        return ""
 
     def estimate(self, data, previous_result: dict = {}) -> dict:
         """
