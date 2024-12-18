e48db91454bf133d8c195d43cb4d7f8874876df7
ranglab <lc_zd@163.com>
2024-12-18 23:33:36 +0800

    update

--- rbt/dmu/__init__.py
+++ rbt/dmu/__init__.py
@@ -2,3 +2,4 @@ from .decision_making_unit import DecisionMakingUnit
 from .md_dmu import MdDMU
 from .spread_dmu import SpreadDMU
 from .mo_intention_dmu import MoIntentionDMU
+from .trend_dmu import TrendDMU
