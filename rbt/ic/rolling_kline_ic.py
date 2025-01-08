ccedcfd7d8f83b4c9a75d98311a4f7b43c91aaa7
ranglab <lc_zd@163.com>
2025-01-08 07:19:13 +0000

    update rbt/ic/rolling_kline_ic.py.

--- rbt/ic/rolling_kline_ic.py
+++ rbt/ic/rolling_kline_ic.py
@@ -6,9 +6,17 @@ class RollingKlineIC(IndexCalculator):
         super().__init__(period)
 
     def calculate(self, new_data):
-        self.result = {
-            "open": self.data[0],
-            "close": self.data[-1],
-            "high": max(self.data),
-            "low": min(self.data),
-        }
+        if len(self.data) > 0:
+            self.result = {
+                "open": self.data[0],
+                "close": self.data[-1],
+                "high": max(self.data),
+                "low": min(self.data),
+            }
+        else:
+            self.result = {
+                "open": new_data,
+                "close": new_data,
+                "high": new_data,
+                "low": new_data,
+            }
