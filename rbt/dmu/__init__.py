e89de0d2728190d8eb3d41d6740114d694598b47
ranglab <lc_zd@163.com>
2025-01-17 08:37:05 +0000

    !28 RBT-32_position_pnl_dmu * update * update * update * update rbt/ic/rolling_kline_ic.py. * update rbt/ic/rolling_kline_ic.py. * update * update * update

--- rbt/dmu/__init__.py
+++ rbt/dmu/__init__.py
@@ -2,3 +2,5 @@ from .decision_making_unit import DecisionMakingUnit
 from .md_dmu import MdDMU
 from .spread_dmu import SpreadDMU
 from .mo_intention_dmu import MoIntentionDMU
+from .trend_dmu import TrendDMU
+from .position_pnl_dmu import PositionPnlDMU
