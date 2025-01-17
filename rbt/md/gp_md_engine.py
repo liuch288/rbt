02e6688970953cb42f1f426c24d519286ca5dd66
liuchang <lc_zd@163.com>
2025-01-17 09:02:08 +0000

    update

--- rbt/md/gp_md_engine.py
+++ rbt/md/gp_md_engine.py
@@ -12,7 +12,6 @@ class GpMdEngine(MdEngine):
         self.db_path = db_path
 
     def prepare_data(self, sym: str, date: datetime.date):
-        self.tick_size = self.get_tick_size(sym)
         data_path = os.path.join(self.db_path, f"gp_tick_{sym}_{str(date)}.csv")
         md = pd.read_csv(data_path)
 
@@ -27,18 +26,3 @@ class GpMdEngine(MdEngine):
 
         # 调用 _register_raw_md 方法注册处理好的数据
         self._register_raw_md(sym, date, md, recover_mo=False)
-
-    def get_tick_size(self, code):
-        if code.startswith("sh"):
-            num_part = code[2:]
-            if num_part.startswith("6"):
-                return 0.01
-            else:
-                return 0.001
-        elif code.startswith("sz"):
-            num_part = code[2:]
-            if num_part.startswith(("00", "30")):
-                return 0.01
-            else:
-                return 0.001
-        return 0.01
