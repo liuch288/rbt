86af1a597f7bc0a654450cdc0da9806cbda84879
liuchang <lc_zd@163.com>
2025-02-06 10:21:48 +0000

    update

--- rbt/peu/simple_biquote_peu.py
+++ rbt/peu/simple_biquote_peu.py
@@ -1,6 +1,5 @@
 from .pnl_estimate_unit import PnlEstimateUnit
 
-
 class SimpleBiquotePEU(PnlEstimateUnit):
     """
     简单双边下单PEU
@@ -16,99 +15,65 @@ class SimpleBiquotePEU(PnlEstimateUnit):
         ask_price_key: str,
         watching_time,
         hands: int = 100,
-        stop_loss: int = 10,
         tick_size: float = 0.001,
     ):
         """
         Args:
             bid_price_key (str): previous_result中保存买单价格的字段的key
-            TODO - 请补全docstring
+            ask_price_key (str): previous_result中保存卖单价格的字段的key
+            watching_time (float): 观察时间
+            hands (int): 手数
+            stop_loss (int): 止损点数
             tick_size (float): 最小报价单位
         """
         super().__init__(watching_time)
         self.bid_price_key = bid_price_key
         self.ask_price_key = ask_price_key
         self.watching_time = watching_time
-        self.tick_size = tick_size
         self.hands = hands
-        self.stop_loss = stop_loss
+        self.tick_size = tick_size
 
         self.digits = len(str(tick_size).split(".")[1])
         self.update_unit_name()
 
     def get_param_str(self):
-        return (
-            f"{self.watching_time}_{self.buy_shift}_{self.sell_shift}_{self.stop_loss}"
-        )
+        # TODO - 请完善这个函数
+        return ""
 
     def estimate(self, future_data, previous_result, *args, **kwargs) -> dict:
-        future_data["exec_px"] = (
-            future_data["trade_notional"] / future_data["trade_sz"] / self.hands
-        )
-        first_md = future_data.iloc[0]
-        start_time = first_md.name
-        buy_px = first_md["bid_px1"] + self.buy_shift * self.tick_size
-        buy_px = round(buy_px, self.digits)
-        sell_px = buy_px + self.sell_shift * self.tick_size
-        sell_px = round(sell_px, self.digits)
-        stop_losing_triggered = False
+        # 从previous_result中获取下单价格
+        buy_px = previous_result[self.bid_price_key]
+        sell_px = previous_result[self.ask_price_key]
 
-        # 判定买单是否立即成交
-        first_md = future_data.iloc[0]
-        if buy_px >= first_md["ask_px1"]:
-            buy_px = first_md["ask_px1"]
-            buy_exec_time = first_md.name
-        else:
-            # 判定买单是否等待后成交
-            md = future_data.iloc[1:]
-            md = future_data.copy()
-            buy_executable = md[
-                (md["ask_px1"] <= buy_px)
-                | ((md["exec_px"] + self.tick_size / 2 < buy_px) & (md["trade_sz"] > 0))
-            ]
-            buy_exec_time = None
-            if len(buy_executable) > 0:
+        # 判定买单是否成交
+        buy_executed = False
+        buy_exec_time = None
+        if buy_px is not None:
+            buy_executable = future_data[future_data["ask_px1"] <= buy_px]
+            if not buy_executable.empty:
+                buy_executed = True
                 buy_exec_time = buy_executable.iloc[0].name
 
-        # 如果判定卖单是否成交
+        # 判定卖单是否成交
+        sell_executed = False
         sell_exec_time = None
-        if buy_exec_time is not None:
-            md = future_data[future_data.index >= buy_exec_time]
-            # 判定卖单是否立即成交
-            first_md = md.iloc[0]
-            if sell_px <= first_md["bid_px1"]:
-                sell_px = first_md["bid_px1"]
-                sell_exec_time = first_md.name
-            else:
-                sell_executable = md[
-                    (md["bid_px1"] >= sell_px)
-                    | (
-                        (md["exec_px"] - self.tick_size / 2 > sell_px)
-                        & (md["trade_sz"] > 0)
-                    )
-                ]
-                if len(sell_executable) > 0:
-                    sell_exec_time = sell_executable.iloc[0].name
+        if sell_px is not None:
+            sell_executable = future_data[future_data["bid_px1"] >= sell_px]
+            if not sell_executable.empty:
+                sell_executed = True
+                sell_exec_time = sell_executable.iloc[0].name
 
-        # 平仓 & 损益统计
+        # TODO - 加上止损逻辑
+
+        # 计算损益
         pnl = 0
-        waiting = -1
-        total_time = -1
-        if (buy_exec_time is not None) and (sell_exec_time is None):
-            last_md = future_data.iloc[-1]
-            pnl = round(last_md["bid_px1"] - buy_px, self.digits)
-        elif (buy_exec_time is not None) and (sell_exec_time is not None):
+        if buy_executed and sell_executed:
             pnl = round(sell_px - buy_px, self.digits)
-            waiting = (sell_exec_time - buy_exec_time).total_seconds()
-            total_time = (sell_exec_time - start_time).total_seconds()
 
         return {
             "pnl": pnl,
-            "waiting": waiting,
-            "total_time": total_time,
+            "buy_executed": buy_executed,
             "buy_exec_time": buy_exec_time,
+            "sell_executed": sell_executed,
             "sell_exec_time": sell_exec_time,
-            "buy_px": buy_px,
-            "sell_px": sell_px,
-            "stop_losing": stop_losing_triggered,
         }
