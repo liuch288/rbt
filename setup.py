03e08e01e5f63c555c706515148ca93f6873b6b3
boat <lc_zd@hotmail.com>
2026-04-16 00:21:24 +0800

    CLQ-94: Update README and setup

--- setup.py
+++ setup.py
@@ -2,17 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name="rbt",
-    version="0.20",
+    version="0.21",
     packages=find_packages(),
     description="RBT framework is a Python library for quantitative trading research, providing functionalities for market data processing, strategy execution, and performance evaluation.",
     author="Your Name",
     author_email="your@email.com",
     url="Your project URL",
     install_requires=[
-        # List the dependencies of the RBT framework
-        "pandas",  # For data processing
-        "numpy",  # For numerical computations
-        "cvxpy",  # For optimization problem solving
+        "pandas",
+        "numpy",
+        "cvxpy",
+        "scipy",
+        "scikit-learn",
+        "progressbar2",
     ],
     entry_points={},
 )
