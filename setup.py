4fbcc9bbd82a4cd11fd03b7e83742c4eebdcf0f0
boat <lc_zd@hotmail.com>
2026-04-23 01:47:08 +0800

    Integrate market-specs package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rbt",
-    version="0.25",
+    version="0.26",
     packages=find_packages(),
     description="RBT framework is a Python library for quantitative trading research, providing functionalities for market data processing, strategy execution, and performance evaluation.",
     author="Your Name",
@@ -15,6 +15,7 @@ setup(
         "scipy",
         "scikit-learn",
         "progressbar2",
+        "market-specs",
     ],
     entry_points={},
 )
